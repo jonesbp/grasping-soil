1603cb80ff2bfc9beb31fd3be706364b02b21a5b
Brian Jones <brian@foamgarden.com>
2025-03-24 08:09:53 -0400

    Add homepage text

--- src/components/HomepageDisplay.svelte
+++ src/components/HomepageDisplay.svelte
@@ -116,47 +116,39 @@
             </div>
             <div class="abstract">
                 <p class="large">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare, massa et vehicula pulvinar,
-                    augue orci interdum lorem, sollicitudin pharetra massa mi quis ex. Sed consequat ex eget tortor
-                    ultrices, non efficitur arcu fringilla. Ut accumsan erat in augue eleifend iaculis. Ut tempus justo
-                    elit, quis blandit est luctus at. Sed et tellus eget sem condimentum egestas. Ut consectetur ex at
-                    vulputate imperdiet. Nunc vulputate arcu massa, et interdum ante tincidunt nec. Quisque imperdiet
-                    ultricies nunc eget sagittis. Mauris consequat ligula vitae nisi facilisis tempus. Fusce cursus
-                    finibus varius. Praesent at dolor sit amet orci maximus ultrices.
+                    Welcome to Grasping Soil, a syllabus and collection of essays that all consider, in some fundamental way: what
+                    comes into view when we consider soil as both a container of human history and point of view for inquiry?
                 </p>
                 <p>
-                    Aliquam consectetur eu ligula quis tempus. Etiam pretium vulputate nulla et vestibulum. Suspendisse
-                    potenti. Vestibulum metus enim, aliquam et massa id, consectetur accumsan eros. Interdum et
-                    malesuada fames ac ante ipsum primis in faucibus. Nam enim nisl, ornare nec iaculis at, laoreet nec
-                    nisi. Cras viverra at arcu a bibendum. Quisque aliquam lectus quam, sed maximus leo dictum a. Etiam
-                    maximus magna vitae nisi sagittis, vitae pharetra ex mollis. Vivamus felis purus, aliquet vitae
-                    ullamcorper vel, eleifend vel velit. Suspendisse rhoncus augue vel nibh vestibulum ultricies. Aenean
-                    rhoncus felis id nulla tristique, nec sodales orci tincidunt. Ut sagittis nibh sed enim condimentum
-                    faucibus. Nam pulvinar turpis ante, a ornare mi accumsan ut. Suspendisse ipsum est, aliquam non
-                    placerat in, aliquam non odio. Etiam consequat molestie arcu, ut fermentum felis dapibus eu.
+                    Soil occupies a nearly ubiquitous presence in our lives regardless of whether we spend much time noticing it. Soil
+                    holds worlds within it and also builds other worlds; it devours and remakes things; it sustains life and gives cover
+                    to the dead. When was the last time you stuck your hands in it or found it under your nails? Your answer probably
+                    reveals something about where you live, your age, your job. You might interact with soil for a living or retreat
+                    to it as an escape from the demands of life. But even if it has been a long time since you last held it in your hands,
+                    can you still recall what soil smells like after it rains? Conjuring that smell might even transport you back to a
+                    very specific moment in your life. In this way, soil is a generous and generative thing -- a material we all draw
+                    on for life as well as a figurative vessel holding deep emotion and connection.
                 </p>
                 <p>
-                    In hac habitasse platea dictumst. In massa ante, pharetra quis erat eget, blandit pretium augue.
-                    Quisque rhoncus ultricies placerat. Vestibulum ac nibh ultricies, feugiat mauris eget, dictum nisi.
-                    Suspendisse ac posuere tortor. Proin luctus finibus mi, id ullamcorper enim interdum vitae. Aenean
-                    tempus, diam quis feugiat hendrerit, ex orci finibus lorem, non semper nisi nibh ac ex. Orci varius
-                    natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed elementum turpis
-                    magna, ut condimentum tellus lobortis eu. Morbi quis tincidunt turpis. Aenean tellus metus,
-                    tristique at rutrum quis, pretium nec arcu. Suspendisse in mauris elit. Nunc lorem metus, mollis non
-                    massa vel, cursus tincidunt est. Nam justo elit, consequat et metus porttitor, hendrerit porttitor
-                    nisi. Fusce cursus maximus egestas. Cras mollis massa vel sollicitudin pellentesque.
+                    Even if actual soil might be invisible in your everyday routines, once you’re attuned to it, you might start
+                    noticing that a language and imagery of it pervades political and cultural life. As you explore the syllabus
+                    you will find links here to books, articles, videos, and art. Please enjoy discovering new things! But beyond
+                    being a repository of links, <a href="/contributors">we</a> have sought to create a syllabus that can actually
+                    be taught (or self-taught), including assignments, discussion questions, writing prompts and forays out into
+                    the world to actually touch soil. As a collective endeavor, there is a delightful and inevitable patchiness to
+                    the final product. Not every section of the syllabus has the same voice and different units might be pitched to different learners.
                 </p>
                 <p>
-                    Phasellus ac fringilla lectus. Fusce eleifend, nisl sed sodales aliquet, diam ipsum porttitor metus,
-                    et pretium arcu quam sit amet nisi. Curabitur non malesuada ipsum. Phasellus cursus tristique
-                    ligula, nec fringilla turpis tincidunt dapibus. Pellentesque fermentum, nisl vitae dignissim
-                    sollicitudin, magna libero convallis dui, id fringilla lacus urna ac est. Nam consequat arcu eu
-                    justo lacinia iaculis. Nunc pretium mi tellus, non pulvinar est maximus eu. Ut aliquam nisl nec urna
-                    laoreet, eu sodales ligula rutrum. Curabitur lorem risus, rutrum non congue ac, convallis nec
-                    libero. Mauris mollis ultrices arcu, a dapibus mi molestie vel. Nulla pulvinar tristique pharetra.
-                    Etiam quis accumsan elit. Integer bibendum justo quis mattis vestibulum. Sed nec lectus lobortis,
-                    placerat mi quis, condimentum nunc. Morbi dignissim congue tellus, id faucibus tortor. Nunc sed
-                    libero lacus.
+                    Where possible, we have linked to open source texts alongside other media. Because of the nature of academic publishing,
+                    that is not always possible. If you would like to get access to something but cannot, <a href="#">contact us</a>. Also, we would
+                    love to hear from you about how you use the syllabus – whether from top to bottom, as something to browse, or if you
+                    borrow chunks of it for your own purposes.
+                </p>
+                <p>
+                    As you explore the syllabus, you will also find links to our essays along the way, which you can also find
+                    <a href="/essays">here</a>. The essays, based on our research interests, explore moments where soil is
+                    both the subject at hand (being assessed, amended, removed, etc) and the vehicle for remaking politics, health,
+                    ideas of home and belonging as well as visions of the future.
                 </p>
             </div>
             <div class="links">
